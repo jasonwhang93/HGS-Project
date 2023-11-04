6c0b27339f322d94d011d3d31e3520e7a30a466c
jasonwhang93 <golddesiner@naver.com>
2023-11-04 22:40:57 +0900

    2023/11/04 QuestMap2 씬 버그 수정

--- HGS Game Project/Assets/Scripts/QuestMap2/Map2CameraFollow.cs
+++ HGS Game Project/Assets/Scripts/QuestMap2/Map2CameraFollow.cs
@@ -2,41 +2,72 @@ using UnityEngine;
 
 public class Map2CameraFollow : MonoBehaviour
 {
-    public Transform player;  // 플레이어의 Transform
-    public float xLeftLimit = -10f;  // 왼쪽 제한
-    public float xRightLimit = 10f;  // 오른쪽 제한
-    public float yTopLimit = 10f;    // 위쪽 제한
-    public float yBottomLimit = -10f; // 아래쪽 제한
+    public Transform player; // 플레이어의 Transform
+    public float xLeftLimit = 1.36f; // 왼쪽 제한
+    public float xRightLimit = 110f; // 오른쪽 제한
+    public float yTopLimit = 20f; // 위쪽 제한
+    public float yBottomLimit = 0.9f; // 아래쪽 제한
     private bool isCameraLocked = false;
 
-    private void LateUpdate()  // LateUpdate를 사용하여 플레이어의 움직임이 모두 처리된 후 카메라를 움직입니다.
+    private Camera mainCamera;
+    private float cameraHalfHeight;
+    private float cameraHalfWidth;
+
+    private void Awake()
+    {
+        mainCamera = Camera.main;
+        cameraHalfHeight = mainCamera.orthographicSize;
+        cameraHalfWidth = cameraHalfHeight * mainCamera.aspect;
+    }
+
+    private void LateUpdate() // LateUpdate를 사용하여 플레이어의 움직임이 모두 처리된 후 카메라를 움직입니다.
     {
         if (player)
         {
-            Vector3 newCameraPosition = new Vector3(player.position.x, player.position.y, transform.position.z);
+            Vector3 newCameraPosition = transform.position;
 
-            // 왼쪽으로 움직일 때
+            // 카메라 이동 제한
+            newCameraPosition.x = Mathf.Clamp(player.position.x, xLeftLimit, xRightLimit);
+            newCameraPosition.y = Mathf.Clamp(player.position.y, yBottomLimit, yTopLimit);
+
+            // 왼쪽 또는 오른쪽으로 움직이려고 할 때 카메라 잠금 상태를 변경
             if (player.position.x < transform.position.x && !isCameraLocked)
             {
                 isCameraLocked = true;
             }
-
-            // 오른쪽으로 움직이려고 할 때
-            if (player.position.x > transform.position.x && isCameraLocked)
+            else if (player.position.x > transform.position.x && isCameraLocked)
             {
                 isCameraLocked = false;
             }
 
+            // 카메라가 잠겼다면 X 위치를 유지합니다.
             if (isCameraLocked)
             {
                 newCameraPosition.x = transform.position.x;
             }
 
-            // 카메라 이동 제한
-            newCameraPosition.x = Mathf.Clamp(newCameraPosition.x, xLeftLimit, xRightLimit);
-            newCameraPosition.y = Mathf.Clamp(newCameraPosition.y, yBottomLimit, yTopLimit);
-
             transform.position = newCameraPosition;
+
+            // 플레이어 위치 제한
+            TargetClamp();
         }
+    }
+
+    void TargetClamp()
+    {
+        // 플레이어의 콜라이더 크기를 구합니다 (BoxCollider2D를 사용한다고 가정)
+        BoxCollider2D playerCollider = player.GetComponent<BoxCollider2D>();
+        float playerWidth = playerCollider.size.x * player.localScale.x / 2; // 플레이어 너비의 절반
+        float playerHeight = playerCollider.size.y * player.localScale.y / 2; // 플레이어 높이의 절반
+
+        float targetMaxX = transform.position.x + cameraHalfWidth - playerWidth;
+        float targetMinX = transform.position.x - cameraHalfWidth + playerWidth;
+        float targetMaxY = transform.position.y + cameraHalfHeight - playerHeight;
+        float targetMinY = transform.position.y - cameraHalfHeight + playerHeight;
+
+        player.position = new Vector3(
+            Mathf.Clamp(player.position.x, targetMinX, targetMaxX),
+            Mathf.Clamp(player.position.y, targetMinY, targetMaxY),
+            player.position.z);
     }
 }
--- HGS Game Project/Assets/Scripts/QuestMap2/Map2MoveController.cs
+++ HGS Game Project/Assets/Scripts/QuestMap2/Map2MoveController.cs
@@ -1,32 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.Tilemaps;
 
 public class Map2MoveController : MonoBehaviour
 {
     public float moveSpeed = 5.0f;  // 이동 속도
-    public float jumpForce = 7.0f;  // 점프 힘
-    private bool isJumping = false;  // 점프 중인지 여부
+    public float jumpForce = 9.0f;  // 점프 힘
+    public GameObject upGround; // UpGround 게임 오브젝트에 대한 참조
+    public GameObject cylinderObject; // CylinderObject에 대한 참조 추가
+
+    private Collider2D upGroundCollider;
+    private Collider2D cylinderObjectCollider; // CylinderObject의 Collider
 
     private Rigidbody2D rb;
     private Animator animator;
     private Vector3 characterScale;  // 캐릭터의 초기 크기 저장
+    private bool isGrounded = false;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
-        characterScale = transform.localScale;
+
+        if (upGround != null)
+        {
+            upGroundCollider = upGround.GetComponent<Collider2D>();
+        }
+        if (cylinderObject != null)
+        {
+            cylinderObjectCollider = cylinderObject.GetComponent<Collider2D>();
+        }
+
+        // 캐릭터의 초기 스케일 설정 (오른쪽을 기본 방향으로 가정)
+        characterScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+        transform.localScale = characterScale; // 오른쪽을 바라보도록 설정
     }
 
     private void Update()
     {
         Move();
         Jump();
+        HandlePlatformEffectorCollision(upGroundCollider); // UpGround 처리
+        HandlePlatformEffectorCollision(cylinderObjectCollider); // CylinderObject 처리
 
         // Animator 변수 업데이트
-        animator.SetBool("isWalk", rb.velocity.x != 0 && !isJumping);
-        animator.SetBool("isJump", isJumping);
+        animator.SetBool("isGrounded", isGrounded);
+        animator.SetBool("isWalk", rb.velocity.x != 0 && isGrounded);
+        animator.SetBool("isJump", !isGrounded);
+        animator.SetBool("isLadder", false);
+        animator.SetBool("isRope", false);
     }
 
     private void Move()
@@ -34,30 +56,69 @@ public class Map2MoveController : MonoBehaviour
         float horizontalMove = Input.GetAxisRaw("Horizontal");
 
         // 캐릭터 방향 변경
-        if (horizontalMove < 0)
-            transform.localScale = characterScale;
-        else if (horizontalMove > 0)
-            transform.localScale = new Vector3(-characterScale.x, characterScale.y, characterScale.z);
+        if (horizontalMove > 0) // 오른쪽으로 이동할 때
+        {
+            transform.localScale = new Vector3(-Mathf.Abs(characterScale.x), characterScale.y, characterScale.z);
+        }
+        else if (horizontalMove < 0) // 왼쪽으로 이동할 때
+        {
+            transform.localScale = new Vector3(Mathf.Abs(characterScale.x), characterScale.y, characterScale.z);
+        }
 
+        // 캐릭터 이동
         Vector2 moveDirection = new Vector2(horizontalMove * moveSpeed, rb.velocity.y);
         rb.velocity = moveDirection;
     }
 
     private void Jump()
     {
-        if (Input.GetButtonDown("Jump") && !isJumping)
+        if (isGrounded && Input.GetButtonDown("Jump"))
         {
             rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
-            isJumping = true;
+            isGrounded = false;
         }
     }
 
+    // HandlePlatformEffectorCollision 메소드를 변경하여 다양한 플랫폼을 처리할 수 있도록 함
+    private void HandlePlatformEffectorCollision(Collider2D platformCollider)
+    {
+        if (CheckIfPlayerIsOnSide(platformCollider))
+        {
+            isGrounded = false;
+            animator.SetBool("isJump", false);
+        }
+    }
+
+    // CheckIfPlayerIsOnSideOfUpGround 메소드를 CheckIfPlayerIsOnSide로 일반화하여 다양한 플랫폼을 처리할 수 있도록 함
+    private bool CheckIfPlayerIsOnSide(Collider2D platformCollider)
+    {
+        Collider2D playerCollider = GetComponent<Collider2D>();
+
+        Bounds playerBounds = playerCollider.bounds;
+        Bounds platformBounds = platformCollider.bounds;
+
+        bool isOnSide = playerBounds.center.x < platformBounds.min.x || playerBounds.center.x > platformBounds.max.x;
+        isOnSide &= playerBounds.center.y > platformBounds.min.y && playerBounds.center.y < platformBounds.max.y;
+
+        return isOnSide;
+    }
+
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        // 플레이어가 땅에 닿으면 다시 점프 가능 상태로 변경
-        if (collision.gameObject.CompareTag("Ground"))
+        // 충돌한 오브젝트가 Ground 또는 UpGround 태그를 가지고 있으면 점프 가능 상태로 변경
+        if (collision.gameObject.CompareTag("Ground") || collision.gameObject.CompareTag("UpGround") || collision.gameObject.CompareTag("Cylinder"))
         {
-            isJumping = false;
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        // Ground 또는 UpGround에서 떨어지면 점프 불가능 상태로 변경
+        if (collision.gameObject.CompareTag("Ground") || collision.gameObject.CompareTag("UpGround") || collision.gameObject.CompareTag("Cylinder"))
+        {
+            isGrounded = false;
         }
     }
 }
